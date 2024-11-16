4753fa16b0bf9c9968a655f1aa359cb622bcda9f
Wilson Lin <code@wilsonl.in>
2024-11-16 07:45:35 +0100

    Do not stitch by level

--- hnsw-sharded-to-roxanne/src/main.rs
+++ hnsw-sharded-to-roxanne/src/main.rs
@@ -1,5 +1,3 @@
-use ahash::HashMap;
-use ahash::HashMapExt;
 use ahash::HashSet;
 use clap::Parser;
 use dashmap::DashMap;
@@ -12,7 +10,7 @@ use libroxanne::common::PointDist;
 use libroxanne::common::StdMetric;
 use libroxanne::db::Db;
 use libroxanne::db::NodeData;
-use libroxanne::hnsw::HnswLevelIndex;
+use libroxanne::hnsw::HnswGraph;
 use libroxanne::search::GreedySearchable;
 use libroxanne::search::Query;
 use libroxanne::vamana::VamanaParams;
@@ -101,110 +99,93 @@ fn main() {
   // - Ensure all IDs are unique across all shards.
   let paths_raw = std::fs::read_to_string(&args.paths).unwrap();
   let paths = paths_raw.lines().collect::<Vec<_>>();
-  // Map from level to (shard path, nodes).
-  let shard_nodes_by_level = DashMap::<usize, Vec<(String, Vec<Id>)>>::new();
   let total_n = AtomicUsize::new(0);
   let seen_ids = DashMap::<Id, String>::new();
   let pb = new_pb(paths.len());
-  paths.par_iter().for_each(|path| {
-    let index = load_hnsw(args.dim, path);
-    let mut map = HashMap::<usize, Vec<Id>>::new();
-    for id in index.labels() {
-      if let Some(other) = seen_ids.insert(id, path.to_string()) {
-        panic!("Duplicate ID {id} in {other} and {path}");
-      };
-      let level = index.get_node_level(id);
-      map.entry(level).or_default().push(id);
-    }
-    for (level, nodes) in map {
-      total_n.fetch_add(nodes.len(), Ordering::Relaxed);
-      shard_nodes_by_level
-        .entry(level)
-        .or_default()
-        .push((path.to_string(), nodes));
-    }
-    pb.inc(1);
-  });
+  let shard_node_counts = paths
+    .par_iter()
+    .map(|path| {
+      let index = load_hnsw(args.dim, path);
+      total_n.fetch_add(index.cur_element_count, Ordering::Relaxed);
+      for id in index.labels() {
+        if let Some(other) = seen_ids.insert(id, path.to_string()) {
+          panic!("Duplicate ID {id} in {other} and {path}");
+        };
+      }
+      pb.inc(1);
+      index.cur_element_count
+    })
+    .collect::<Vec<_>>();
   let total_n = total_n.load(Ordering::Relaxed);
   pb.finish();
   println!("Verified shards");
 
   let pb = new_pb(total_n);
-  let mut medoid: Option<Id> = None;
   // Map from base node ID to map from shard path to other shard node ID.
   let cliques = DashMap::<Id, DashMap<String, Id>>::new();
   // Map from base node ID to combined out-neighbors.
   // We don't need a set, as a node cannot have an out-neighbor that also exists in another shard.
   let clique_neighbors = DashMap::<Id, Vec<Id>>::new();
   let node_to_clique = DashMap::<Id, Id>::new();
-  // We want to go level-by-level as nodes on different levels have different edge length and count distributions, which is probably ideal to preserve.
-  for (level, mut shards) in shard_nodes_by_level
-    .into_iter()
-    .sorted_unstable_by_key(|e| e.0)
-    .rev()
-  {
-    // Use the shard with the most nodes at the current level as the base, as otherwise there may be some leftover nodes in other shards and the code/algorithm gets messy if we want to ensure they get used too. (Shards may not be perfectly balanced.)
-    // Recalculate base shard for every level as different levels may have different largest shards.
-    shards.sort_unstable_by_key(|e| Reverse(e.1.len()));
 
-    let (base_file, base_level_nodes) = &shards[0];
-    let base = load_hnsw(args.dim, base_file);
-    let base_graph = HnswLevelIndex::new(&base, metric, level, base_level_nodes);
-    // We can't just pick the entry point as the start that may not exist on our level.
-    let base_path = base_graph.find_shortest_spanning_tree(base_level_nodes[0]);
-    if medoid.is_none() {
-      medoid = Some(base.entry_label());
-    };
+  // Use the shard with the most nodes as the base, as otherwise there may be some leftover nodes in other shards and the code/algorithm gets messy if we want to ensure they get used too. (Shards may not be perfectly balanced.)
+  let shards = (0..paths.len())
+    .sorted_unstable_by_key(|&i| Reverse(shard_node_counts[i]))
+    .map(|i| paths[i])
+    .collect_vec();
 
-    base_level_nodes.par_iter().for_each(|&id| {
-      assert!(cliques.insert(id, DashMap::new()).is_none());
-      assert!(clique_neighbors
-        .insert(id, base.get_merged_neighbors(id, 0).into_iter().collect())
-        .is_none());
-      assert!(node_to_clique.insert(id, id).is_none());
+  let base_file = shards[0];
+  let base = load_hnsw(args.dim, base_file);
+  let medoid = base.entry_label();
+  let base_graph = HnswGraph::new(&base, metric, None);
+  let base_path = base_graph.find_shortest_spanning_tree(medoid);
+
+  for id in base_graph.ids() {
+    cliques.insert(id, DashMap::new());
+    clique_neighbors.insert(id, base_graph.get_out_neighbors(id).0.clone());
+    node_to_clique.insert(id, id);
+  }
+  pb.inc(base.cur_element_count as u64);
+
+  for batch in (&shards[1..]).chunks(args.parallel - 1) {
+    batch.into_par_iter().for_each(|&path| {
+      let other = load_hnsw(args.dim, path);
+      let other_graph = HnswGraph::new(&other, metric, None);
+      let mut available = other_graph.ids().collect::<HashSet<_>>();
+
+      for (base_id_from, base_id) in base_path.iter().cloned() {
+        let base_vec = base_graph.get_point(base_id);
+
+        let other_node = cliques
+          // If None: we're at the start.
+          .get(&base_id_from)
+          // If None: no eqivalent to `from` in this shard was found previously.
+          .and_then(|c| c.get(path).map(|e| *e))
+          // We'll just use any point still available as the start. If there's not even a point available, we'll have to skip this shard.
+          // NOTE: We cannot just use the HNSW entry node as it isn't available on this level (unless we're at the top level).
+          .or_else(|| available.iter().cloned().next())
+          .and_then(|start| {
+            other_graph.greedy_search_fast1(Query::Vec(&base_vec.view()), start, |n| {
+              available.contains(&n)
+            })
+          });
+        let Some(PointDist { id: other_node, .. }) = other_node else {
+          continue;
+        };
+
+        assert!(available.remove(&other_node));
+        cliques
+          .get_mut(&base_id)
+          .unwrap()
+          .insert(path.to_string(), other_node);
+        clique_neighbors
+          .get_mut(&base_id)
+          .unwrap()
+          .extend(other_graph.get_out_neighbors(other_node).0);
+        assert!(node_to_clique.insert(other_node, base_id).is_none());
+        pb.inc(1);
+      }
     });
-    pb.inc(base_level_nodes.len() as u64);
-
-    for batch in (&shards[1..]).chunks(args.parallel - 1) {
-      batch.into_par_iter().for_each(|(path, shard_level_nodes)| {
-        let other = load_hnsw(args.dim, path);
-        let other_graph = HnswLevelIndex::new(&other, metric, level, &shard_level_nodes);
-        let mut available = shard_level_nodes.iter().cloned().collect::<HashSet<_>>();
-
-        for (base_id_from, base_id) in base_path.iter().cloned() {
-          let base_vec = base_graph.get_point(base_id);
-
-          let other_node = cliques
-            // If None: we're at the start.
-            .get(&base_id_from)
-            // If None: no eqivalent to `from` in this shard was found previously.
-            .and_then(|c| c.get(path).map(|e| *e))
-            // We'll just use any point still available as the start. If there's not even a point available, we'll have to skip this shard.
-            // NOTE: We cannot just use the HNSW entry node as it isn't available on this level (unless we're at the top level).
-            .or_else(|| available.iter().cloned().next())
-            .and_then(|start| {
-              other_graph.greedy_search_fast1(Query::Vec(&base_vec.view()), start, |n| {
-                available.contains(&n)
-              })
-            });
-          let Some(PointDist { id: other_node, .. }) = other_node else {
-            continue;
-          };
-
-          assert!(available.remove(&other_node));
-          cliques
-            .get_mut(&base_id)
-            .unwrap()
-            .insert(path.clone(), other_node);
-          clique_neighbors
-            .get_mut(&base_id)
-            .unwrap()
-            .extend(other.get_merged_neighbors(other_node, 0));
-          assert!(node_to_clique.insert(other_node, base_id).is_none());
-          pb.inc(1);
-        }
-      });
-    }
   }
   let nodes_touched = pb.position();
   pb.finish();
@@ -221,12 +202,12 @@ fn main() {
 
   db.write_cfg(&cfg);
   db.write_dim(args.dim);
-  db.write_medoid(medoid.unwrap());
+  db.write_medoid(medoid);
   db.write_metric(args.metric);
 
   let pb = new_pb(total_n);
   for path_batch in paths.chunks(args.parallel) {
-    path_batch.into_par_iter().for_each(|path| {
+    path_batch.into_par_iter().for_each(|&path| {
       let index = load_hnsw(args.dim, path);
       // Collect to Vec so we can use into_par_iter, which is much faster than par_bridge.
       index.labels().collect_vec().into_par_iter().for_each(|id| {
