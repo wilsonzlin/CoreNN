c3d7541963ec2dd1c3277b3e414581c400d86d76
Wilson Lin <code@wilsonl.in>
2024-11-16 08:26:24 +0100

    Implement DB keys

--- libroxanne/src/db.rs
+++ libroxanne/src/db.rs
@@ -1,20 +1,24 @@
 use crate::common::Id;
 use crate::common::StdMetric;
 use crate::pq::ProductQuantizer;
-use crate::vamana::VamanaParams;
 use bitcode::Decode;
 use bitcode::Encode;
+use rmp_serde::to_vec_named;
 use rocksdb::BlockBasedOptions;
+use rocksdb::DBPinnableSlice;
 use rocksdb::Direction;
 use rocksdb::IteratorMode;
+use rocksdb::WriteBatchWithTransaction;
 use serde::Serialize;
 use std::path::Path;
-use std::str::from_utf8;
 
 #[derive(PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
-pub enum DbKey {
-  Cfg,
+pub enum DbKeyT {
+  CfgDegreeBound,
+  CfgDistanceThreshold,
+  CfgUpdateBatchSize,
+  CfgUpdateSearchListCap,
   Deleted, // (Id)
   Dim,
   Id,  // (Vec<u8>)
@@ -27,6 +31,124 @@ pub enum DbKey {
   PqModel,
   PqVec, // (Id)
   TempIndexOffsets,
+}
+
+pub trait DbKey {
+  fn bytes(&self) -> Vec<u8>;
+}
+
+impl DbKey for DbKeyT {
+  fn bytes(&self) -> Vec<u8> {
+    vec![*self as u8]
+  }
+}
+
+impl DbKey for (DbKeyT, Id) {
+  fn bytes(&self) -> Vec<u8> {
+    let mut res = vec![self.0 as u8];
+    res.extend_from_slice(&self.1.to_le_bytes());
+    res
+  }
+}
+
+impl<'a> DbKey for (DbKeyT, &'a [u8]) {
+  fn bytes(&self) -> Vec<u8> {
+    let mut res = vec![self.0 as u8];
+    res.extend_from_slice(self.1);
+    res
+  }
+}
+
+impl<'a> DbKey for (DbKeyT, &'a str) {
+  fn bytes(&self) -> Vec<u8> {
+    let mut res = vec![self.0 as u8];
+    res.extend_from_slice(self.1.as_bytes());
+    res
+  }
+}
+
+pub struct DbTransaction {
+  batch: WriteBatchWithTransaction<false>,
+}
+
+impl DbTransaction {
+  pub fn new() -> Self {
+    DbTransaction {
+      batch: WriteBatchWithTransaction::default(),
+    }
+  }
+
+  fn write_raw(&mut self, k: impl DbKey, v: Vec<u8>) {
+    self.batch.put(k.bytes(), v);
+  }
+
+  fn write(&mut self, k: impl DbKey, v: &impl Serialize) {
+    self.write_raw(k, to_vec_named(v).unwrap());
+  }
+
+  pub fn write_cfg_degree_bound(&mut self, dim: usize) {
+    self.write(DbKeyT::CfgDegreeBound, &dim);
+  }
+
+  pub fn write_cfg_distance_threshold(&mut self, dist: f64) {
+    self.write(DbKeyT::CfgDistanceThreshold, &dist);
+  }
+
+  pub fn write_cfg_update_batch_size(&mut self, size: usize) {
+    self.write(DbKeyT::CfgUpdateBatchSize, &size);
+  }
+
+  pub fn write_cfg_update_search_list_cap(&mut self, cap: usize) {
+    self.write(DbKeyT::CfgUpdateSearchListCap, &cap);
+  }
+
+  pub fn write_deleted(&mut self, id: Id) {
+    self.write_raw((DbKeyT::Deleted, id), Vec::new());
+  }
+
+  pub fn write_dim(&mut self, dim: usize) {
+    self.write(DbKeyT::Dim, &dim);
+  }
+
+  pub fn write_id(&mut self, key: &str, id: Id) {
+    self.write((DbKeyT::Id, key), &id);
+  }
+
+  pub fn write_key(&mut self, id: Id, key: &str) {
+    self.write_raw((DbKeyT::Key, id), key.as_bytes().to_vec());
+  }
+
+  pub fn write_medoid(&mut self, medoid: Id) {
+    self.write(DbKeyT::Medoid, &medoid);
+  }
+
+  pub fn write_metric(&mut self, metric: StdMetric) {
+    self.write(DbKeyT::Metric, &metric);
+  }
+
+  pub fn write_next_id(&mut self, id: Id) {
+    self.write(DbKeyT::NextId, &id);
+  }
+
+  pub fn write_node(&mut self, id: Id, node: &NodeData) {
+    self.write_raw((DbKeyT::Node, id), node.serialize());
+  }
+
+  pub fn write_node_count(&mut self, count: usize) {
+    self.write(DbKeyT::NodeCount, &count);
+  }
+
+  pub fn write_pq_model(&mut self, pq: &ProductQuantizer<f32>) {
+    self.write(DbKeyT::PqModel, pq);
+  }
+
+  pub fn write_pq_vec(&mut self, id: Id, vec: Vec<u8>) {
+    self.write_raw((DbKeyT::PqVec, id), vec);
+  }
+
+  pub fn commit(self, db: &Db) {
+    db.db.write(self.batch).unwrap();
+  }
 }
 
 // We store both in one DB entry to leverage one disk page read to get both, as specified in the DiskANN paper. (If we store them as separate DB entries, they are unlikely to be stored in the same disk page.) We don't have to store the embedding.
@@ -66,6 +188,7 @@ fn rocksdb_options(create: bool) -> rocksdb::Options {
   let mut bbt_opt = BlockBasedOptions::default();
   // Use close to a single page size to avoid excessive reads as that counters design of NodeData (seek DiskANN paper).
   bbt_opt.set_block_size(1024 * 4);
+  // DiskANN explicitly avoids dependence on memory caches, so in the same spirit disable this.
   bbt_opt.disable_cache();
   bbt_opt.set_format_version(6);
   opt.set_block_based_table_factory(&bbt_opt);
@@ -91,93 +214,110 @@ impl Db {
     &self.db
   }
 
-  pub fn iter_nodes(&self) -> impl Iterator<Item = (Id, NodeData)> + '_ {
-    self
-      .db
-      .full_iterator(IteratorMode::From(b"node/", Direction::Forward))
-      .map(|e| e.unwrap())
-      .take_while(|(k, _)| k.starts_with(b"node/"))
-      .map(|(k, v)| {
-        let id_raw = k.strip_prefix(b"node/").unwrap();
-        let id = Id::from_str_radix(from_utf8(id_raw).unwrap(), 10).unwrap();
-        let node = NodeData::deserialize(&v);
-        (id, node)
-      })
-  }
-
   pub fn flush(&self) {
     self.db.flush().unwrap();
   }
 
-  fn write_raw(&self, k: impl AsRef<str>, v: Vec<u8>) {
-    let k = k.as_ref().to_string().into_bytes();
-    self.db.put(k, v).unwrap();
+  fn iter<'a, T>(
+    &'a self,
+    kt: DbKeyT,
+    parser: impl Fn(&[u8]) -> T + 'a,
+  ) -> impl Iterator<Item = (Id, T)> + 'a {
+    self
+      .db
+      .full_iterator(IteratorMode::From(&[kt as u8], Direction::Forward))
+      .map(|e| e.unwrap())
+      .take_while(move |(k, _)| k[0] == (kt as u8))
+      .map(move |(k, v)| {
+        let id_raw = &k[1..];
+        let id = Id::from_le_bytes(id_raw.try_into().unwrap());
+        (id, parser(&v))
+      })
   }
 
-  fn write(&self, k: impl AsRef<str>, v: &impl Serialize) {
-    self.write_raw(k, rmp_serde::to_vec_named(v).unwrap());
+  pub fn iter_nodes(&self) -> impl Iterator<Item = (Id, NodeData)> + '_ {
+    self.iter(DbKeyT::Node, |v| NodeData::deserialize(v))
   }
 
-  pub fn read_cfg(&self) -> VamanaParams {
-    let raw = self.db.get_pinned("cfg").unwrap().unwrap();
+  fn maybe_read_raw(&self, k: impl DbKey) -> Option<DBPinnableSlice<'_>> {
+    self.db.get_pinned(k.bytes()).unwrap()
+  }
+
+  fn read_raw(&self, k: impl DbKey) -> DBPinnableSlice<'_> {
+    self.maybe_read_raw(k).unwrap()
+  }
+
+  pub fn read_cfg_degree_bound(&self) -> usize {
+    let raw = self.read_raw(DbKeyT::CfgDegreeBound);
     rmp_serde::from_slice(&raw).unwrap()
   }
 
-  pub fn write_cfg(&self, cfg: &VamanaParams) {
-    self.write("cfg", cfg);
+  pub fn read_cfg_distance_threshold(&self) -> f64 {
+    let raw = self.read_raw(DbKeyT::CfgDistanceThreshold);
+    rmp_serde::from_slice(&raw).unwrap()
+  }
+
+  pub fn read_cfg_update_batch_size(&self) -> usize {
+    let raw = self.read_raw(DbKeyT::CfgUpdateBatchSize);
+    rmp_serde::from_slice(&raw).unwrap()
+  }
+
+  pub fn read_cfg_update_search_list_cap(&self) -> usize {
+    let raw = self.read_raw(DbKeyT::CfgUpdateSearchListCap);
+    rmp_serde::from_slice(&raw).unwrap()
+  }
+
+  pub fn read_deleted(&self, id: Id) -> bool {
+    self.maybe_read_raw((DbKeyT::Deleted, id)).is_some()
   }
 
   pub fn read_dim(&self) -> usize {
-    let raw = self.db.get_pinned("dim").unwrap().unwrap();
+    let raw = self.read_raw(DbKeyT::Dim);
     rmp_serde::from_slice(&raw).unwrap()
   }
 
-  pub fn write_dim(&self, dim: usize) {
-    self.write("dim", &dim);
+  pub fn read_id(&self, key: &str) -> Id {
+    let raw = self.read_raw((DbKeyT::Id, key));
+    rmp_serde::from_slice(&raw).unwrap()
+  }
+
+  pub fn read_key(&self, id: Id) -> String {
+    let raw = self.read_raw((DbKeyT::Key, id));
+    String::from_utf8(raw.to_vec()).unwrap()
   }
 
   pub fn read_medoid(&self) -> Id {
-    let raw = self.db.get_pinned("medoid").unwrap().unwrap();
+    let raw = self.read_raw(DbKeyT::Medoid);
     rmp_serde::from_slice(&raw).unwrap()
-  }
-
-  pub fn write_medoid(&self, medoid: Id) {
-    self.write("medoid", &medoid);
   }
 
   pub fn read_metric(&self) -> StdMetric {
-    let raw = self.db.get_pinned("metric").unwrap().unwrap();
+    let raw = self.read_raw(DbKeyT::Metric);
     rmp_serde::from_slice(&raw).unwrap()
   }
 
-  pub fn write_metric(&self, metric: StdMetric) {
-    self.write("metric", &metric);
+  pub fn read_next_id(&self) -> Id {
+    let raw = self.read_raw(DbKeyT::NextId);
+    rmp_serde::from_slice(&raw).unwrap()
   }
 
   pub fn read_node(&self, id: Id) -> NodeData {
-    let raw = self.db.get_pinned(format!("node/{id}")).unwrap().unwrap();
+    let raw = self.read_raw((DbKeyT::Node, id));
     NodeData::deserialize(&raw)
   }
 
-  pub fn write_node(&self, id: Id, node: &NodeData) {
-    self.write_raw(format!("node/{id}"), node.serialize());
-  }
-
-  pub fn read_pq_model(&self) -> ProductQuantizer<f32> {
-    let raw = self.db.get_pinned("pq_model").unwrap().unwrap();
+  pub fn read_node_count(&self) -> usize {
+    let raw = self.read_raw(DbKeyT::NodeCount);
     rmp_serde::from_slice(&raw).unwrap()
   }
 
-  pub fn write_pq_model(&self, pq: &ProductQuantizer<f32>) {
-    self.write("pq_model", pq);
+  pub fn read_pq_model(&self) -> ProductQuantizer<f32> {
+    let raw = self.read_raw(DbKeyT::PqModel);
+    rmp_serde::from_slice(&raw).unwrap()
   }
 
   pub fn read_pq_vec(&self, id: Id) -> Vec<u8> {
-    let raw = self.db.get_pinned(format!("pq_vec/{id}")).unwrap().unwrap();
+    let raw = self.read_raw((DbKeyT::PqVec, id));
     raw.to_vec()
-  }
-
-  pub fn write_pq_vec(&self, id: Id, vec: Vec<u8>) {
-    self.write_raw(format!("pq_vec/{id}"), vec);
   }
 }
