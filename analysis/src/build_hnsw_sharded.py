fa4aa2def67819fdbcbd06aa71d9d4ef25d3aa54
Wilson Lin <code@wilsonl.in>
2024-11-16 03:54:19 +0000

    Fix build_hnsw_sharded

--- analysis/src/build_hnsw_sharded.py
+++ analysis/src/build_hnsw_sharded.py
@@ -1,18 +1,26 @@
-from util import read_vectors
+import argparse
+from pathlib import Path
 import hnswlib
 import numpy as np
 import os
-import pathlib
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description="Build HNSW index shards.")
+parser.add_argument("--m", type=int, help="M hyperparameter")
+parser.add_argument("--ef", type=int, help="ef hyperparameter")
+parser.add_argument("--dim", type=int, help="Dimension of vectors")
+args = parser.parse_args()
 
 dataset = os.environ["DS"]
+out_dir = f"hnsw-sharded-{args.m}M-{args.ef}ef"
 
-print("Loading")
-mat = read_vectors(f"dataset/{dataset}/base.fvecs", np.float32)
-n, dim = mat.shape
+Path(f"dataset/{dataset}/out/{out_dir}/indices").mkdir(parents=True, exist_ok=True)
 
-pathlib.Path(f"dataset/{dataset}/out/hnsw-sharded/indices").mkdir(
-    parents=True, exist_ok=True
-)
+print("[build_hnsw_sharded] Loading")
+mat = np.frombuffer(
+    open(f"dataset/{dataset}/vectors.bin", "rb").read(), np.float32
+).reshape((-1, args.dim))
+n, _ = mat.shape
 
 ids = np.arange(n)
 np.random.seed(42)
@@ -21,12 +29,12 @@ shards = 50
 assert n % shards == 0
 shard_size = n // shards
 
-for i in range(0, shards):
+for i in tqdm(range(shards)):
     shard_ids = ids[i * shard_size : (i + 1) * shard_size]
-    index = hnswlib.Index("l2", dim)
-    index.init_index(max_elements=shard_size, ef_construction=133, M=80)
-    print("Indexing", i)
+    index = hnswlib.Index("l2", args.dim)
+    index.init_index(max_elements=shard_size, ef_construction=args.ef, M=args.m)
+    print("[build_hnsw_sharded] Indexing", i)
     index.add_items(mat[shard_ids, :], shard_ids)
-    print("Saving", i)
-    index.save_index(f"dataset/{dataset}/out/hnsw-sharded/indices/shard{i}.hnsw")
+    print("[build_hnsw_sharded] Saving", i)
+    index.save_index(f"dataset/{dataset}/out/{out_dir}/indices/shard{i}.hnsw")
 print("All done!")
